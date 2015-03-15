47e47c7c67df35ed12d6fb1401f9a1affeaefc55
stvyzxx <yurasobechko@gmail.com>
2015-03-15 13:55:11 +0200

    update2

--- js/script.js
+++ js/script.js
@@ -5,61 +5,62 @@ function fixedMenu(){
 
 	if(document.body.clientWidth > 700){
 		
+		window.addEventListener("scroll", onScrollMenu, false);
 
-		addEvent(window, "scroll", onScrollMenu);
-		function onScrollMenu(){
-			var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-			var wrapper = document.body;
-			var header = document.getElementsByTagName("header")[0];
-			var nav  = document.getElementById("navigation");
-			var temp, timer, i = 0;
-			if(scrollTop>200){
-				if(freeze){
-					freeze = 0;
-					temp = header.removeChild(nav);
-					temp.setAttribute("class", "fixedMenu");					
-					temp.getElementsByTagName("ul")[0].appendChild(callback());  
-					wrapper.insertBefore(temp, wrapper.firstChild);					
-					
-					timer = setInterval(function(){
-						animate();
-						if(i>=1){
-							clearInterval(timer);
-							i = 0;
-						}							
-					}, 30);
-				}
+	}
+	
+	function onScrollMenu(){
+		var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		var wrapper = document.body;
+		var header = document.getElementsByTagName("header")[0];
+		var nav  = document.getElementById("navigation");
+		var temp, timer, i = 0;
+		if(scrollTop>200){
+			if(freeze){
+				freeze = 0;
+				temp = header.removeChild(nav);
+				temp.setAttribute("class", "fixedMenu");					
+				temp.getElementsByTagName("ul")[0].appendChild(callback());  
+				wrapper.insertBefore(temp, wrapper.firstChild);					
 				
-			}else if(scrollTop<=200){
-				freeze = 1;
-				if(wrapper.getElementsByClassName("fixedMenu")[0]){
-					temp = wrapper.getElementsByClassName("fixedMenu")[0];
-					var callLinkDel = temp.children[0].lastChild;					
-					temp.getElementsByTagName("ul")[0].removeChild(callLinkDel);				
-					temp.removeAttribute("class");
-					
-					header.insertBefore(temp, header.getElementsByClassName("tags-block")[0]);					
-				}					
-			}
-
-			//------- animate function			
-			function animate(){
-				wrapper.getElementsByClassName("fixedMenu")[0].style.opacity = i;
-				i+=0.1		
+				timer = setInterval(function(){
+					animate();
+					if(i>=1){
+						clearInterval(timer);
+						i = 0;
+					}							
+				}, 30);
 			}
 			
-			//------- callback function
-			function callback(){
-				var callLi =  document.createElement("li");
-				var callLink = document.createElement("a");
-				callLink.setAttribute("id","fixedCallLink");
-				callLink.setAttribute("href","#");
-				callLink.innerHTML = "Передзвонити?";
-				callLi.appendChild(callLink);
-				return callLi;
-			}
-			
+		}else if(scrollTop<=200){
+			freeze = 1;
+			if(wrapper.getElementsByClassName("fixedMenu")[0]){
+				temp = wrapper.getElementsByClassName("fixedMenu")[0];
+				var callLinkDel = temp.children[0].lastChild;					
+				temp.getElementsByTagName("ul")[0].removeChild(callLinkDel);				
+				temp.removeAttribute("class");
+				
+				header.insertBefore(temp, header.getElementsByClassName("tags-block")[0]);					
+			}					
 		}
+		
+		//------- animate function			
+		function animate(){
+			wrapper.getElementsByClassName("fixedMenu")[0].style.opacity = i;
+			i+=0.1		
+		}
+		
+		//------- callback function
+		function callback(){
+			var callLi =  document.createElement("li");
+			var callLink = document.createElement("a");
+			callLink.setAttribute("id","fixedCallLink");
+			callLink.setAttribute("href","#");
+			callLink.innerHTML = "Передзвонити?";
+			callLi.appendChild(callLink);
+			return callLi;
+		}
+		
 	}
 }	
 
